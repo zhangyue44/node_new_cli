cad0e8d9b8e66c72dfab3144cc7c1114ac6a407c
zhangyue44 <zy1617768774@163.com>
2022-04-20 21:47:04 +0800

    fix: 添加-v

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { program } = require("commander");
 
-const helpOptions = require('./lib/core/help') // 定义选项
-const createCommands = require('./lib/core/create') // 配置命令
+const helpOptions = require("./lib/core/help"); // 定义选项
+const createCommands = require("./lib/core/create"); // 配置命令
 
-program.version(require('./package.json').version) // 动态查看版本号
+// program.version(require('./package.json').version) // 动态查看版本号
+program.version(require("./package.json").version, "-v, --version"); // 动态查看版本号
 
-helpOptions()
-createCommands()
+helpOptions();
+createCommands();
 
-program.parse(process.argv) // 解析终端指令
+program.parse(process.argv); // 解析终端指令
